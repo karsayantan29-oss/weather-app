2ccb9bff901c2dd0598c7522c4a296834098f170
unknown <karsayantan29@gmail.com>
2025-10-14 17:13:37 +0530

    Fix API key and remove local build files

--- src/apikeys.js
+++ src/apikeys.js
@@ -1,6 +1,7 @@
 const apiKeys = {
-  key: process.env.REACT_APP_API_KEY,
+  key: process.env.I9JU23NF394R6HH,
   base: "https://api.openweathermap.org/data/2.5/",
 };
-console.log("🔑 API Key loaded:", process.env.REACT_APP_API_KEY);
+console.log("🔑 API Key loaded:", process.env.I9JU23NF394R6HH);
+
 export default apiKeys;
