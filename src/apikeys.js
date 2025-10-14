c0564e475a9ce2e827e0bd63a9442bae12d02313
unknown <karsayantan29@gmail.com>
2025-10-14 16:17:42 +0530

    Debug: log API key from env

--- src/apikeys.js
+++ src/apikeys.js
@@ -2,5 +2,5 @@ const apiKeys = {
   key: process.env.REACT_APP_API_KEY,
   base: "https://api.openweathermap.org/data/2.5/",
 };
-
+console.log("🔑 API Key loaded:", process.env.REACT_APP_API_KEY);
 export default apiKeys;
